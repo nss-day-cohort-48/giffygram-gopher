bb46867a219f807dba1ac67d6fb8de9b8eac7c65
Ashton Monzi <anmonzi@gmail.com>
2021-05-20 09:52:36 -0500

    created fetch requests and synced up modules to render login component

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,23 @@
+import { LoginForm } from "./auth/Login.js"
+import { fetchPosts, fetchUsers } from "./data/provider.js"
 import { GiffyGram } from "./GiffyGram.js"
+
 
 const applicationElement = document.querySelector(".giffygram")
 
 export const renderApp = () => {
     const user = parseInt(localStorage.getItem("gg_user"))
 
-    if (user) {
-        applicationElement.innerHTML = GiffyGram()
-    } else {
-        applicationElement.innerHTML = LoginForm()
-    }
+    // Call fetches here before any html loads
+    fetchUsers().then(fetchPosts).then(
+        () => {
+            if (user) {
+                applicationElement.innerHTML = GiffyGram()
+            } else {
+                applicationElement.innerHTML = LoginForm()
+            }
+        }
+    )
 }
+
+renderApp()
