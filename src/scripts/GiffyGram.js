51942a92dd698298167f98ac535342b5b9ba806f
CodyJones4235 <codyjones4325@gmail.com>
2021-05-20 15:27:11 -0500

    Displaying posts '

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -1,5 +1,16 @@
-export const GiffyGram = () => {
+import {postList} from "./feed/PostList.js"
 
-    // Show main main UI
-    return `<h1>Giffygram</h1>`
+export const GiffyGram = () => {
+    return `<h1>Giffygram</h1>
+    <nav class="navigation"></nav>
+
+    <article class="giffygram__feed">
+        <div class="newPost"></div>
+
+        <section class="post">${postList()}</section>
+    </article>
+
+    <footer class="footer"></footer>
+    `
+
 }
--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -1,12 +1,47 @@
-const apiURL = "http://localhost:8088"
+const apiURL = "http://localhost:3000"
 const applicationElement = document.querySelector(".giffygram")
 
 
 const applicationState = {
+    users: [],
+    posts: [],
+    directMessages: [],
+    favorites: [],
+    follows: [],
     currentUser: {},
     feed: {
         chosenUser: null,
         displayFavorites: false,
         displayMessages: false
     }
+}
+
+
+
+// Fetch requests
+export const fetchUsers = () => {
+    return fetch(`${apiURL}/users`)
+    .then(response => response.json())
+    .then(userData => {
+        applicationState.users = userData
+    })
+}
+
+export const fetchPosts = () => {
+    return fetch(`${apiURL}/posts`)
+    .then(response => response.json())
+    .then(postData => {
+        applicationState.posts = postData
+    })
+}
+
+
+
+// GETTER functions
+export const getUsers = () => {
+    return [...applicationState.users]
+}
+
+export const getPosts = () => {
+    return [...applicationState.posts]
 }
